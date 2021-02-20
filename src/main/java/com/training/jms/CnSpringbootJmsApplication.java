87304d710c30a27293ed5ec4c64b0eafec64af68
cnoriega20 <c79.noriega@gmail.com>
2021-02-20 02:02:20 -0600

    Added ActiveMQ embedded server and logging level.

--- src/main/java/com/training/jms/CnSpringbootJmsApplication.java
+++ src/main/java/com/training/jms/CnSpringbootJmsApplication.java
@@ -1,12 +1,25 @@
 package com.training.jms;
 
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CnSpringbootJmsApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        // Setting up an embedded ActiveMQ server
+        ActiveMQServer activeMQServer = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+                .setPersistenceEnabled(false)
+                .setJournalDirectory("target/data/journal")
+                .setSecurityEnabled(false)
+                .addAcceptorConfiguration("invm", "vm://0"));
+
+        activeMQServer.start();
+
         SpringApplication.run(CnSpringbootJmsApplication.class, args);
     }
 
