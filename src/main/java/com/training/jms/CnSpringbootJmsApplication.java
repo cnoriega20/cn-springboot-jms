9fc0bbc0013fda50dcf2c07808c57c996e142133
cnoriega20 <c79.noriega@gmail.com>
2021-02-20 01:02:01 -0600

    Initial commit from Spring Initialzr.

---
+++ src/main/java/com/training/jms/CnSpringbootJmsApplication.java
@@ -0,0 +1,13 @@
+package com.training.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnSpringbootJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnSpringbootJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/training/jms/CnSpringbootJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.jms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnSpringbootJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
