415ee0f9d76cf3d71eb8432f201f08b188fa2582
cnoriega20 <c79.noriega@gmail.com>
2021-02-21 12:44:51 -0600

    Reverted dependencies. Added fix for Artemis Startup failure.

--- src/main/java/com/training/jms/CnSpringbootJmsApplication.java
+++ src/main/java/com/training/jms/CnSpringbootJmsApplication.java
@@ -1,24 +1,41 @@
 package com.training.jms;
 
+import org.apache.activemq.artemis.api.core.TransportConfiguration;
+import org.apache.activemq.artemis.core.config.Configuration;
 import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.remoting.impl.invm.InVMAcceptorFactory;
+import org.apache.activemq.artemis.core.remoting.impl.netty.NettyAcceptorFactory;
 import org.apache.activemq.artemis.core.server.ActiveMQServer;
 import org.apache.activemq.artemis.core.server.ActiveMQServers;
+import org.apache.activemq.artemis.core.server.impl.ActiveMQServerImpl;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+import java.util.HashSet;
+
+@SpringBootApplication//(exclude = ArtemisAutoConfiguration.class)
+@EnableConfigurationProperties(ArtemisProperties.class)
 public class CnSpringbootJmsApplication {
 
     public static void main(String[] args) throws Exception {
 
         // Setting up an embedded ActiveMQ server
-        ActiveMQServer activeMQServer = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
-                .setPersistenceEnabled(false)
-                .setJournalDirectory("target/data/journal")
-                .setSecurityEnabled(false)
-                .addAcceptorConfiguration("invm", "vm://0"));
 
-        activeMQServer.start();
+        Configuration config = new ConfigurationImpl();
+        HashSet<TransportConfiguration> transports = new HashSet<TransportConfiguration>();
+
+        transports.add(new TransportConfiguration(NettyAcceptorFactory.class.getName()));
+        transports.add(new TransportConfiguration(InVMAcceptorFactory.class.getName()));
+
+        config.setAcceptorConfigurations(transports);
+
+
+        ActiveMQServer server = new ActiveMQServerImpl(config);
+        server.start();
 
         SpringApplication.run(CnSpringbootJmsApplication.class, args);
     }
