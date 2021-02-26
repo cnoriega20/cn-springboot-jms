7f6e1d1cb014e4403f7d96d59b7a0ab3dc5ed9d0
cnoriega20 <c79.noriega@gmail.com>
2021-02-26 00:33:45 -0600

    Add new listener method.

--- src/main/java/com/training/jms/listener/ExampleMessageListener.java
+++ src/main/java/com/training/jms/listener/ExampleMessageListener.java
@@ -2,8 +2,10 @@ package com.training.jms.listener;
 
 import com.training.jms.config.JmsConfig;
 import com.training.jms.model.ExampleMessage;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -15,28 +17,30 @@ import java.util.UUID;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class ExampleMessageListener {
 
     private Object ExampleMessage;
+    private final JmsTemplate jmsTemplate;
 
     @JmsListener(destination = JmsConfig.QUEUE)
     public void listen(@Payload ExampleMessage exampleMessage,
                        @Headers MessageHeaders headers, Message message) {
         log.info("Got a message");
-        log.info("example Message",exampleMessage);
+        log.info("example Message", exampleMessage);
 
 
     }
 
     @JmsListener(destination = JmsConfig.SEND_RECEIVE_QUEUE)
     public void listenForCustomMessage(@Payload ExampleMessage exampleMessage,
-                       @Headers MessageHeaders headers, Message message) throws JMSException {
+                                       @Headers MessageHeaders headers, Message message) throws JMSException {
 
-        ExampleMessage paylodMsg = ExampleMessage
-                .builder()
+        com.training.jms.model.ExampleMessage payloadMsg = com.training.jms.model.ExampleMessage.builder()
                 .id(UUID.randomUUID())
-                .message("Send and Receive")
+                .message("Sending from listenForCustomMessage...")
                 .build();
 
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
     }
 }
